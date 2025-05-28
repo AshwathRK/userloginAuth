2a4ae85cb79b411aa1ff26c091b111a11c2067ea
Ashwathaman <ashwathaman.r6@gmail.com>
2025-05-28 23:29:52 +0530

    db commit

--- db.js
+++ db.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/users', {
+const uri = `mongodb+srv://ashwathamanr6:0EqI3qLt4i8f1ix3@cluster0.82ryhpa.mongodb.net/`;
+
+
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error('MongoDB connection error:', err));
