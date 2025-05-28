d68af57ea85670704ac7a179426617c2ceeb4f2e
Ashwathaman <ashwathaman.r6@gmail.com>
2025-05-28 23:57:01 +0530

    CORS status 1

--- index.js
+++ index.js
@@ -18,17 +18,17 @@ HTTP_Server.set('view engine', 'ejs');
 HTTP_Server.set('views', path.join(__dirname, 'views'));
 
 HTTP_Server.use(express.static('public'));
+HTTP_Server.use('/', router);     // ✅ Routes come after middlewares
 HTTP_Server.use(bodyParser.json());
 HTTP_Server.use(bodyParser.urlencoded({ extended: false }));
 
-HTTP_Server.use(cookieParser());  // ✅ Correct order
-HTTP_Server.use(auth);            // ✅ Middleware that uses cookies
+HTTP_Server.use(cookieParser()); 
+HTTP_Server.use(auth);
 
 //Swagger configurations
 const swaggerSpec = swaggerJsdoc(swaggerOptions);
 HTTP_Server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-HTTP_Server.use('/', router);     // ✅ Routes come after middlewares
 
 HTTP_Server.listen(3000, (err) => {
     if (err) {
